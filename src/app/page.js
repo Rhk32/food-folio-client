489e4664288dfd3761d793fb6bd22562d41522cb
raf0670 <swagbondhu@gmail.com>
2026-08-05 22:26:48 +0600

    dummy user data fetched

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,17 @@ import Logo from '../../public/foodFolioLogoCircular.png'
 
 const Home = async () => {
 	const res = await fetch(process.env.NEXT_PUBLIC_API_URL);
-	const uuid = await res.json();
+	const users = await res.json();
+	// console.log(res);
+	// console.log(users);
 
 	return (
 		<div className='max-w-7xl mx-auto text-center space-y-6'>
-			<h1>If everytime you get a different uuid then its ok</h1>
-			<p>{uuid}</p>
 			<h1 className='font-bold text-7xl'>LOGO</h1>
 			<Image loading='eager' src={Logo} alt='' width={500} height={500}></Image>
+			{
+				users.map(user => <h1 key={user.id}>{user.name}</h1>)
+			}
 		</div>
 	);
 };
