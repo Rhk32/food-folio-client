b73e8d5080197dd276821b7bcf32b5f087afb909
raf0670 <swagbondhu@gmail.com>
2026-08-09 16:13:02 +0600

    login design done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = async () => {
 	const health = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`);
 	const healthMessage = await health.json();
-	console.log(healthMessage);
+	// console.log(healthMessage);
 	return (
 		<></>
 	);
